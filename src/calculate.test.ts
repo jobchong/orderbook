7e96d2a3911196474896b60148f3af552b0c3172
Job Chong <jobchong@gmail.com>
2023-08-28 20:05:39 +0800

    Fix calculate test type errors

--- src/calculate.test.ts
+++ src/calculate.test.ts
@@ -4,15 +4,17 @@ import { jest } from '@jest/globals'
 
 jest.mock('./redis');
 
+client.mGet = (jest.fn(() => Promise.resolve(['100', '200', '300'])) as unknown) as typeof client.mGet;
+
 describe('calculateGlobalMidPrice', () => {
   it('should calculate average correctly', async () => {
-    (client.mGet as jest.Mock).mockResolvedValue(['100', '200', '300']);
+    (client.mGet as unknown as jest.MockedFunction<() => Promise<string[]>>).mockResolvedValue(['100', '200', '300']);
     const result = await calculateGlobalMidPrice();
     expect(result).toBe(200);
   });
 
   it('should handle Redis failure', async () => {
-    (client.mGet as jest.Mock).mockRejectedValue(new Error('Redis Error'));
+    (client.mGet as unknown as jest.MockedFunction<() => Promise<Error>>).mockRejectedValue(new Error('Redis Error'));
     await expect(calculateGlobalMidPrice()).rejects.toThrow('Redis Error');
   });
 });
